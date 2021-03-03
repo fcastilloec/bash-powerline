83e26ac561e47bd65f89e4cadf1ff7f9ee5f9445
Felipe Castillo <fcastillo.ec@gmail.com>
2021-03-02 22:50:47 -0800

    feat: add path_separator and fix shellcheck errors

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# forked from https://github.com/riobard/bash-powerline
+# shellcheck disable=2001
+
+# forked from https://github.com/cyberdork33/bash-powerline
 __powerline() {
 
   ## USER CONFIG ##
   readonly max_path_depth=3
+  readonly path_separator='/'
 
   ## standard fonts
   # ▷ ◣ ▶︎ ➤ 〉  $ % ⑂ + ⇡ ⇣
@@ -78,80 +81,92 @@ __powerline() {
   }
 
   __git_info() {
-    if (__is_git_branch); then
+    local branch
+    local marks
+    local stat
+    local aheadN
+    local behindN
 
-      # get current branch name or short SHA1 hash for detached head
-      local branch="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
-      [ -n "$branch" ] || return  # git branch not found
+    # get current branch name or short SHA1 hash for detached head
+    branch="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
+    [ -n "$branch" ] || return  # git branch not found
 
-      local marks
+    # branch is modified?
+    [ -n "$(git status --porcelain)" ] && marks+=" $git_branch_changed_symbol"
 
-      # branch is modified?
-      [ -n "$(git status --porcelain)" ] && marks+=" $git_branch_changed_symbol"
+    # how many commits local branch is ahead/behind of remote?
+    stat="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
+    aheadN="$(echo "$stat" | grep -o 'ahead \d\+' | grep -o '\d\+')"
+    behindN="$(echo "$stat" | grep -o 'behind \d\+' | grep -o '\d\+')"
+    [ -n "$aheadN" ] && marks+=" $git_need_push_symbol$aheadN"
+    [ -n "$behindN" ] && marks+=" $git_need_pull_symbol$behindN"
 
-      # how many commits local branch is ahead/behind of remote?
-      local stat="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
-      local aheadN="$(echo $stat | grep -o 'ahead \d\+' | grep -o '\d\+')"
-      local behindN="$(echo $stat | grep -o 'behind \d\+' | grep -o '\d\+')"
-      [ -n "$aheadN" ] && marks+=" $git_need_push_symbol$aheadN"
-      [ -n "$behindN" ] && marks+=" $git_need_pull_symbol$behindN"
-
-      # print the git branch segment without a trailing newline
-      printf "$git_branch_symbol$branch$marks"
-    fi
+    # print the git branch segment without a trailing newline
+    printf "$git_branch_symbol $branch$marks"
   }
 
-  # guidance from https://github.com/Hexcles/bash-powerline
-  __getpwd() {
-    local my_pwd=$(pwd)
+  __get_pwd() {
+    local my_pwd
+    local split
+
+    my_pwd=$(pwd)
 
     # substitude ~ for $HOME
-    local my_pwd=$(sed -e "s|^$HOME|~|" <<< $my_pwd)
+    my_pwd=$(sed -e "s|^$HOME|~|" <<< "$my_pwd")
 
-    # get an array of directories
-    # local split=(${my_pwd//\// })
-    # ???
-    local oIFS=$IFS
     local IFS='/'
     read -ra split <<< "$my_pwd"
-    IFS=$oIFS
 
     # count how many backslashes
     local pathDepth=${#split[@]}
 
     # calculate index of last element
-    lastPosition=$((pathDepth-1))
+    local lastPosition=$((pathDepth-1))
 
-    # Substitude '  ' for instances of '/'
-    my_pwd=$(sed -e "s|/|  |g" <<< $my_pwd)
+    # Substitute '/' for user's visual path separator
+    [[ "$path_separator" != '/' ]] && my_pwd=$(sed -e "s|/|$path_separator|g" <<< "$my_pwd")
 
     if [[ $my_pwd = ~* ]]; then
       # shorten path if more than 2 directories lower than home
       if [[ $pathDepth > $max_path_depth ]]; then
-        my_pwd="~  ...  ${split[lastPosition]}"
+        my_pwd="~$path_separator...$path_separator${split[lastPosition]}"
       fi
     else
-      # In other than home, shorten path when greater than 3
-      # directories deep.
+      # In other than home, shorten path when greater than 3 directories deep.
       if [[ $pathDepth > $max_path_depth ]]; then
-        my_pwd="/  ${split[1]}   ...   ${split[lastPosition]}"
+        [[ $path_separator != '/' ]] && my_pwd="/$path_separator" || my_pwd='/'
+        my_pwd+="${split[1]}$path_separator...$path_separator${split[lastPosition]}"
       else
         # append a backslash to the front of pwd
-        my_pwd=$(sed -e "s|^|\/ |" <<< $my_pwd)
+        [[ "$path_separator" != '/' ]] && my_pwd=$(sed -e "s|^|/|" <<< "$my_pwd")
       fi
     fi
     echo "$my_pwd"
   }
 
   ps1() {
-    # Check the exit code of the previous command and display different
-    # colors in the prompt accordingly.
-    if [ $? -ne 0 ]; then
-      local bg_exit="$bg_red"
-      local fg_exit="$fg_red"
+    # Saves exit code of last run command
+    exit_code=$?
+
+    # Check if we're on a git directory and store it on is_git. 0 is true, any other value is false
+    __is_git_branch
+    is_git=$?
+
+    local bg_exit
+    local fg_exit
+
+    # Display different colors in the prompt according to exit_code
+    if [ $exit_code -ne 0 ]; then
+      bg_exit="$bg_red"
+      fg_exit="$fg_red"
     else
-      local bg_exit="$bg_green"
-      local fg_exit="$fg_green"
+      if [[ $is_git -eq 0 ]]; then
+        bg_exit="$bg_base03"
+        fg_exit="$fg_base03"
+      else
+        bg_exit="$bg_base00"
+        fg_exit="$fg_base00"
+      fi
     fi
 
     PS1=""
@@ -166,13 +181,13 @@ __powerline() {
     fi
 
     # path
-    PS1+="$bg_base00$fg_base3 $(__getpwd) $reset_colors"
+    PS1+="$bg_base00$fg_base3 $(__get_pwd) $reset_colors"
 
     # git status
-    if (__is_git_branch); then
-      PS1+="$bg_base01$fg_base00$prompt_divider$reset_colors"
-      PS1+="$bg_base01$fg_base1 $(__git_info) $reset_colors"
-      PS1+="$bg_exit$fg_base01$prompt_divider$reset_colors"
+    if [[ $is_git -eq 0 ]]; then
+      PS1+="$bg_base03$fg_base00$prompt_divider$reset_colors"
+      PS1+="$bg_base03$fg_base02 $(__git_info) $reset_colors"
+      PS1+="$bg_exit$fg_base03$prompt_divider$reset_colors"
     else
       PS1+="$bg_exit$fg_base00$prompt_divider$reset_colors"
     fi
